6dfbe1e530fb56e41533bfb45d84142f49d49437
Andrej Boleslavsky <yer@atlas.cz>
2022-04-05 20:18:32 +0200

    Fixed URL prediction

--- mintPrediction-UAWarline.py
+++ mintPrediction-UAWarline.py
@@ -6,11 +6,34 @@
 
 #Website
 #https://metahistory.gallery/
+
 import json
 import os
 from web3 import Web3
 import time
 import requests
+
+def getTokenNFT(_token):
+    reqSess = requests.Session()  
+    
+    _url = 'https://ipfs.io/ipfs/QmU3gHF45sRXbyxojhWEJV1m6t5QFk7DuCi4LeSPK87QJ6/' + str(_token)
+    try:
+        r = reqSess.get(url=_url,timeout=10) 
+        usr = r.json()
+        return usr['item_number']
+    except Exception as e:
+        return 0
+
+def getTokenImageURL(_token):
+    reqSess = requests.Session()  
+    
+    _url = 'https://ipfs.io/ipfs/QmU3gHF45sRXbyxojhWEJV1m6t5QFk7DuCi4LeSPK87QJ6/' + str(_token)
+    try:
+        r = reqSess.get(url=_url,timeout=10) 
+        usr = r.json()
+        return usr['image'].replace('ipfs://','https://ipfs.io/ipfs/')
+    except Exception as e:
+        return 0
 
 def getTokenNFT(_token):
     reqSess = requests.Session()  
@@ -34,15 +57,21 @@ warlineTokenContract = w3.eth.contract(address=warlineAddress, abi=warlineABI)
 def printGasInfo():
     print("Gas price in wei:" + str(w3.fromWei(int(w3.eth.gasPrice*1), 'gwei')))
 
+_maxTokens = warlineTokenContract.functions.maxTokens().call() 
+
 _oldID = -1
 while (True):
     time.sleep(1)
     _currentID = warlineTokenContract.functions.totalSupply().call()
+    if _currentID >= _maxTokens:
+        print("Minting concluded, all tokens minted")
+        break
     if _currentID != _oldID:
         _nftID = int(getTokenNFT(_currentID))
         if _nftID != 0:
             _oldID = _currentID
-
-        print("Next to be minted: " + str(_nftID +1) + " ")
-        print("Image URL: " +"https://ipfs.io/ipfs/QmWYTbSse2cUNQo1FBpeAS9S7scU8H5655MYSuwFuT5wUE/" +str(_nftID +1) + ".jpg")
+        print("Next NFT #" + str(_currentID+1))
+        print("Next design ID to be minted: " + str(_nftID +1) + " ")
+        imageURL = getTokenImageURL(_currentID - 98)
+        print("Image URL: " + imageURL)        
         printGasInfo()
